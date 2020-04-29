58d859206a6caa4a2307a73195e48d96baf2c054
Dapro718 <60823817+Dapro718@users.noreply.github.com>
2020-04-29 08:11:48 -0400

    Update EventListener.php

--- src/Dapro718/WordScramble/EventListener.php
+++ src/Dapro718/WordScramble/EventListener.php
@@ -27,29 +27,31 @@ class EventListener implements Listener {
   }
   
   public function onChat(PlayerChatEvent $event) {
-  #  $this->plugin->getServer()->info("Chat Event Activated");
     $scrambledWord = $this->plugin->sWord;
     $correctWord = $this->plugin->cWord;
-  #  $this->plugin->getLogger()->info("Scrambled is: $scrambledWord");
-  #  $this->plugin->getLogger()->info("Correct is: $correctWord");
     $player = $event->getPlayer();
     $displayname = $event->getPlayer()->getName();
     $money = $this->plugin->pMoney;
     $message = strtolower($event->getMessage());
-  #  $this->plugin->getLogger()->info("Players message is: $message");
     if($event->getPlayer() instanceof Player) {
-   #   $this->plugin->getLogger()->info("Player is $player and is an instance of Player");
-      if($correctWord !== "closed902312409") {
-    #    $this->plugin->getLogger()->info("Word is open");
-    #    $this->plugin->getLogger()->info("$message = $scrambledWord");
-        if($message === $correctWord) {
-     #     $this->plugin->getLogger()->info("message is correct, things should happen");
-          EconomyAPI::getInstance()->addMoney($player, $money);
-          $this->plugin->getServer()->broadcastMessage("§e{$displayname} §2has unscrambled §e{$correctWord} §2from §2{$scrambledWord} and got §e\${$money}§2!");
-          $this->plugin->closeWord();
+      if(!$player->hasPermission("wordscramble.deny")) {
+        if($correctWord !== "closed902312409") {
+          if($message === $correctWord) {
+            if($player->hasPermission("wordscramble.bonus")) {
+              EconomyAPI::getInstance()->addMoney($player, $money * 2);
+              $this->plugin->getServer()->broadcastMessage("§e{$displayname} §2has unscrambled §e{$correctWord} §2from §2{$scrambledWord} and got §e\${$money}§2!");
+              $this->plugin->closeWord();
+            } else {
+            EconomyAPI::getInstance()->addMoney($player, $money * 2);
+            $this->plugin->getServer()->broadcastMessage("§e{$displayname} §2has unscrambled §e{$correctWord} §2from §2{$scrambledWord} and got §e\${$money}§2!");
+            $this->plugin->closeWord();
+            }
+          }
+        } else {
+          return TRUE;
         }
       } else {
-        return TRUE;
+        // do nothing
       }
     }
   }
